8c053c12dc01c64e597b0f90c3b21292ed502957
yogitashete2412 <yogitashete24@gmail.com>
2023-05-21 20:08:05 +0530

    intro added

---
+++ Day-1 Introduction to CPP/index.cpp
@@ -0,0 +1,19 @@
+//What is Programming??
+//Programming is the process of instructions that tell a computer how to perform a task.
+
+// Why Learn C++?
+// C++ is used to develop games, desktop apps, operating systems, browsers, and so on.
+// C++ Executes very fast.
+// Rich Library support.
+// Machine Independent & Platform Dependent language.
+// C++ allows you to divide complex problems into smaller sets by using objects.
+
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello world!";
+
+    return 0;
+}
